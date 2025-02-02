3fdda217886fcc9cec06a0ce7550984c101ec5f3
Gabriel Vieira <ing.gabrielvieira@gmail.com>
2025-02-02 11:52:12 +0100

    chat container bubble

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set)=>({
+export const useChatStore = create((set, get)=>({
     messages:[],
     users:[],
     selectedUser:null,
@@ -21,7 +21,7 @@ export const useChatStore = create((set)=>({
         }
       },
 
-      getMessages: async (userId) => {
+    getMessages: async (userId) => {
         set({ isMessagesLoading: true });
         try {
           const res = await axiosInstance.get(`/messages/${userId}`);
@@ -31,7 +31,17 @@ export const useChatStore = create((set)=>({
         } finally {
           set({ isMessagesLoading: false });
         }
-      },
+    },
+
+    sendMessages: async(messageData) =>{
+      const {selectedUser, messages} = get()
+      try {
+        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+        set({messages:[...messages, res.data]});
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
 
     setSelectedUser: (selectedUser) => set({selectedUser}),
 }));
